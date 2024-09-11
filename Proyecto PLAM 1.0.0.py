fd22e896eb78b18432f06ff0459027e10b7abe09
lisaramos1 <lisaramos@uade.edu.ar>
2024-09-11 16:34:42 -0300

    proyecto plam

--- Proyecto PLAM 1.0.0.py
+++ Proyecto PLAM 1.0.0.py
@@ -9,7 +9,25 @@ while uso==0:
     print('0 Salir')
     funcion = int(input())
     if funcion==1:
-        DiscosStock.menudiscos()    
+        loop=0
+        while loop==0:
+            print('ingrese un numero del 0 al 4')
+            print('1: Agregar Discos ➕')
+            print('2: Modificar Discos ⚙')
+            print('3: Eliminar Discos ✖')
+            print('4: Ver Stock 👀')
+            print('0: Volver')
+            menu = int(input())
+            if menu==1:
+                DiscosStock.agregar()
+            elif menu==2:
+                DiscosStock.modificar()
+            elif menu==3:
+                DiscosStock.eliminar()
+            elif menu==4:
+                DiscosStock.mostrar()
+            elif menu==0:
+                loop=1
     elif funcion==2:
         Personas.menue()
     if funcion==3:
