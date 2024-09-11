702ee64e2dded56e4c7410df8eebd6ee2d5dd2b9
acedenosanchez <acedenosanchez@uade.edu.ar>
2024-09-11 16:44:37 -0300

    funciones varias y menu prestamos

--- Proyecto PLAM 1.0.0.py
+++ Proyecto PLAM 1.0.0.py
@@ -31,7 +31,29 @@ while uso==0:
     elif funcion==2:
         Personas.menue()
     if funcion==3:
-        prestamos.crud_prestamos()
+        loop=0
+    while loop == 0 :
+        print('1 Crear prestamos ➕')
+        print('2 Modificar prestamos ➖')
+        print('3 Eliminar prestamo ⚙️')
+        print('4 Mostrar listado 👀')
+        print("0 volver")
+        menu = int(input('Ingrese una acción:' ))
+        if menu==1:
+            NroCliente=int(input("Ingrese el numero de cliente: ")) 
+            Album=input("Ingrese el nombre del album: ")
+            Diasdeprestamos=int(input("Ingrese cuantos dias se realizara el prestamo: "))
+            prestamos.crear_prestamos(NroCliente,Album,Diasdeprestamos)
+        if menu==2:
+            prestamos.modificar_prestamos() 
+        if menu==3:
+            prestamos.eliminar_prestamos()
+        if menu==4:
+            prestamos.mostrar_prestamos() 
+        if menu == 0:
+            loop=1
+        else : 
+            print("Ingrese un numero valido")
         
     else:
         uso=1
